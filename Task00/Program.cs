273a634078e568b6b73722a805240fd4ca162c45
SpotRus <spotik56rus@gmail.com>
2023-09-20 16:21:16 +0300

    Added example 1

--- Task00/Program.cs
+++ Task00/Program.cs
@@ -1,1 +1,15 @@
-﻿
+﻿// Напишите программу, которая: 
+// 1. На вход принимает число 
+// 2. Выдаёт его квадрат (число умноженное на само себя).
+// 3. Выводит результат.
+
+// Например:
+// 4 -> 16
+// -3 -> 9
+// -7 -> 49
+
+Console.Write("Введите целое число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int square = number * number;
+// Console.WriteLine(number + " -> " + square);
+Console.WriteLine($"{number} -> {square}");
