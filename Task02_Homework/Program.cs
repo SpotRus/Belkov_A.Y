a879b83bdb7a7c48e3bf7c665ad460b17a881fa7
SpotRus <spotik56rus@gmail.com>
2023-09-23 13:57:00 +0300

    Added Task06 Homework

--- Task02_Homework/Program.cs
+++ Task02_Homework/Program.cs
@@ -9,8 +9,10 @@
 
 Console.Write("Введите первое число: ");
 int firstNumber = Convert.ToInt32(Console.ReadLine());
+
 Console.Write("Введите второе число: ");
 int secondNumber = Convert.ToInt32(Console.ReadLine());
+
 
 if (firstNumber > secondNumber)
 {
