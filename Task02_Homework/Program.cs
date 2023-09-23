19a8421aeb2d6cc988cb41c6c5c895fd8c1bf90e
SpotRus <spotik56rus@gmail.com>
2023-09-23 13:34:17 +0300

    Correction Task02

--- Task02_Homework/Program.cs
+++ Task02_Homework/Program.cs
@@ -7,7 +7,7 @@
 // a = 2 b = 10 -> max = 10
 // a = -9 b = -3 -> max = -3
 
-Console.Write("Введите первое целое число: ");
+Console.Write("Введите первое число: ");
 int firstNumber = Convert.ToInt32(Console.ReadLine());
 Console.Write("Введите второе число: ");
 int secondNumber = Convert.ToInt32(Console.ReadLine());
