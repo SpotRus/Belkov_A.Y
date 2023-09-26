c94c70bf69f8b484238e5adb13ce452e5970846d
SpotRus <spotik56rus@gmail.com>
2023-09-26 14:35:09 +0300

    Added Task15 Homework

---
+++ Task15_Homework/Program.cs
@@ -0,0 +1,27 @@
+﻿// Напишите программу, которая: 
+// 1. Принимает на вход цифру, обозначающую день недели.
+// 2. Проверяет, является ли этот день выходным.
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+
+bool Weekend(int num)
+{
+    if (num == 6 || num == 7)
+    {
+        Console.WriteLine($"{num} -> да");
+        return true;
+    }
+    Console.WriteLine($"{num} -> нет");
+    return false;
+}
+
+Console.Write("Введите цифру дня недели: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number >= 1 && number <=7)
+{
+    Console.WriteLine(Weekend(number));
+} 
+else
+    Console.WriteLine("Некоректный ввод.");
