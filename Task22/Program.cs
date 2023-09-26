4ae6e7cbceb584c6d26d6ab6d05c187666953a2d
SpotRus <spotik56rus@gmail.com>
2023-09-26 14:38:41 +0300

    Added Task22

---
+++ Task22/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу, которая:
+// 1. Принимает на вход число (N). 
+// 2. Выдаёт таблицу квадратов чисел от 1 до N.
+
+void SquareNumbers (int num) 
+{
+    int count = 1;
+    while (count <= num) {
+        Console.WriteLine($"{count, 3} -> {count*count, 5}");
+        count++;
+    }
+}
+
+Console.WriteLine ("Digit input from user:");
+int userNum = Convert.ToInt32(Console.ReadLine());
+SquareNumbers(userNum);
