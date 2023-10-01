9b80b54e40f8201de8f30317bdab9122cd48c6f8
SpotRus <spotik56rus@gmail.com>
2023-10-01 17:12:38 +0300

    Added Task 26

---
+++ Task26/Program.cs
@@ -0,0 +1,32 @@
+﻿// Напишите программу, которая: 
+// 1. Принимает на вход число.
+// 2. Выдаёт количество цифр в числе.
+
+// Например:
+// 456 -> 3
+// 78 -> 2
+// 89126 -> 5
+
+int GetUserInput(string msg)
+{
+    Console.Write($"{msg}: ");
+    int num = Convert.ToInt32(Console.ReadLine());
+    return num;
+}
+
+int LengthOfNumber(int num)
+{
+    if (num == 0) return 1;
+    int count = 0;
+    while (num != 0)
+    {
+        count++;
+        num = num / 10;
+    }
+    return count;
+}
+
+int number = GetUserInput("Введите число");
+int length = LengthOfNumber(number);
+
+Console.WriteLine($"{number} -> {length}");
