b6034f143ca39efee52aae8efc53e4b8176f7450
SpotRus <spotik56rus@gmail.com>
2023-10-03 12:32:49 +0300

    Added Task 27 Homework

---
+++ Task27_Homework/Program.cs
@@ -0,0 +1,30 @@
+﻿// Напишите программу, которая: 
+// 1. Принимает на вход число.
+// 2. Выдаёт сумму цифр в числе.
+
+// Например:
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+int GetUserInput(string msg)
+{
+    Console.Write($"{msg}: ");
+    int num = Convert.ToInt32(Console.ReadLine());
+    return num;
+}
+
+int SumDigits(int num)
+{
+    int result = 0;
+    while (num > 0)
+    {
+        result += num % 10;
+        num /= 10;
+    }
+    return result;
+}
+
+int number = GetUserInput("Введите число: ");
+
+Console.WriteLine($"{number} -> {SumDigits(number)}");
