68a2473b6784931e6e2d2b4b8da6365ade8e262e
SpotRus <spotik56rus@gmail.com>
2023-10-03 12:33:04 +0300

    Added Task 29 Homework

---
+++ Task29_Homework/Program.cs
@@ -0,0 +1,29 @@
+﻿
+
+void RandomArray(int[] array)
+{
+    Random rand = new Random();
+    int count = array.Length;
+    int index = 0;
+    while (index < count)
+    {
+        array[index] = rand.Next(1, 100);
+        index++;
+    }
+}
+
+void PrintArray(int[] printArray)
+{
+    string output = String.Empty;
+    for (int i = 0; i < printArray.Length; i++)
+    {
+        output += (i < printArray.Length - 1) ? $"{printArray[i]}, " : $"{printArray[i]}";
+        
+    }
+    Console.WriteLine($"{output} -> [{output}]");
+}
+
+int[] array = new int[8];
+
+RandomArray(array);
+PrintArray(array);
