23e2eeed9dacd845ec9bef81b9d6f2e8dc8a4bc6
SpotRus <spotik56rus@gmail.com>
2023-10-23 16:30:42 +0300

    Added Task 67

---
+++ Task67/Program.cs
@@ -0,0 +1,23 @@
+﻿// Напишите программу:
+// 1. Принимает на вход число.
+// 2. Возвращать сумму его цифр.
+
+// Например:
+// 453 -> 12
+// 45 -> 9
+
+int GetInputUser(string msg)
+{
+    Console.Write(msg);
+    int num = Convert.ToInt32(Console.ReadLine());
+    return num;
+}
+
+int SumOfDigits(int num)
+{
+    if (num == 0) return 0;
+    return num % 10 + SumOfDigits(num / 10);
+}
+
+int userInput = GetInputUser("Введите число: ");
+Console.WriteLine($"{userInput} -> {SumOfDigits(userInput)}");//453 45 4
