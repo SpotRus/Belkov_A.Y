57e8dd3fbccfa016d2aa0d50d68d54f1298f3cb9
SpotRus <spotik56rus@gmail.com>
2023-10-23 16:30:00 +0300

    Added Task 63

---
+++ Task63/Program.cs
@@ -0,0 +1,21 @@
+﻿// 1. Задайте значение N.
+// 2. Напишите программу, которая выведет все натуральные числа в промежутку от 1 до N.
+
+string CounterPlus(int n)
+{
+
+    return (n != 0) ? CounterPlus(n - 1) + $"{n,2} " : String.Empty;
+}
+
+int GetInputUser(string msg)
+{
+    Console.Write(msg);
+    int num = Convert.ToInt32(Console.ReadLine());
+    return num;
+}
+
+int userInput = GetInputUser("Введите натуральное число: ");
+
+if (userInput < 0) Console.WriteLine("Некорректный ввод.");
+
+Console.WriteLine(CounterPlus(userInput));
